6d53e08d5de2a972d5833fed51bcec9830b4dabb
javierMoret <javier.s257989@cesurformacion.com>
2022-02-13 19:47:36 +0100

    gwg

--- Gestion de Equiposss/src/gestionequipo/Jugador.java
+++ Gestion de Equiposss/src/gestionequipo/Jugador.java
@@ -1,43 +1,56 @@
 package gestionequipo;
 
+
+ /**
+ * Esta clase define jugadores 
+ * @author: Equipo1DAW
+ * @version 13/02/2022/A
+ */
+
+
+
 public class Jugador {
+	// Atributos de la clase
 	private String nombre;
 	private int edad;
 	private String idioma;
+	
+    /**
+     * Constructor para la serie Jugador
+     */
 
 	public Jugador() {
 
 	}
+	//Cierre del constructor
+	
+	 /**
+
+     * Método que sirve para asignarle un nombre a un jugador
+     * @param nombre El parámetro nombre define el nombre del jugador
+
+     */
 
 	public void setNombreJugador(String nombre) {
-		boolean esNulo = false;
 		int caracterMax = 20;
-		int valorInicial = 0;
-		int enteroNull = -1;
 
-		if (nombre == null) {
-			this.nombre = null;
-		} else {
-			nombre = nombre.toUpperCase();
-			if (nombre.isEmpty()) {
-				esNulo = true;
-			}
-			if (nombre.length() > caracterMax) {
-				esNulo = true;
-			}
-			for (int i = valorInicial; i <= nombre.length() + enteroNull; i++) {
-				char letra = nombre.charAt(i);
-				if (!Character.isLetter(letra) && letra != 32) {
-					esNulo = true;
+		this.nombre = null;
+		if (nombre != null) {
+			if (nombre.length() <= caracterMax) {
+				if (nombre.matches("[a-zA-Z]+")) {
+					this.nombre = nombre.toUpperCase();
 				}
 			}
-			if (esNulo) {
-				nombre = null;
-			}
-			this.nombre = nombre;
 		}
-
 	}
+	//Cierre del metodo
+	
+	 /**
+
+     * Método que sirve para asignarle una edad a un jugador
+     * @param edad El parámetro edad define la edad del jugador
+
+     */
 
 	public void setEdad(int edad) {
 		int edadMin = 18;
@@ -49,36 +62,36 @@ public class Jugador {
 		}
 		this.edad = edad;
 	}
+	//Cierre del metodo
+	
+	 /**
+
+     * Método que sirve para asignarle un idioma a un jugador
+     * @param idioma El parámetro idioma define el idioma del jugador
+
+     */
 
 	public void setIdioma(String idioma) {
-		if (idioma == null) {
-			this.idioma = null;
-		} else {
+
+		this.idioma = null;
+		if (idioma != null) {
 			if (idioma.equalsIgnoreCase("espanol") || idioma.equalsIgnoreCase("frances")
 					|| idioma.equalsIgnoreCase("aleman") || idioma.equalsIgnoreCase("ingles")) {
-
-			} else {
-				idioma = null;
+				this.idioma = idioma;
 			}
-			this.idioma = idioma;
 		}
 
 	}
+	//Cierre del metodo
+	
+	 /**
 
-	public String getIdioma() {
-		return idioma;
-	}
+     * Método que sirve para obtener la categoria del jugador a partir de la edad del jugador
+	 * @return La categoria a la que pertenece el jugador
 
-	public String getNombreJugador() {
-		return nombre;
-	}
-
-	public int getEdad() {
-		return edad;
-	}
+     */
 
 	public String tipoJugador() {
-		int enteroNull = -1;
 		int edadJuniorMin = 18;
 		int edadJuniorMax = 25;
 		int edadSeniorMin = 26;
@@ -88,7 +101,7 @@ public class Jugador {
 		int edadSuperMasterMin = 100;
 		String categoria = null;
 
-		if (nombre != null && edad != enteroNull && idioma != null) {
+		if (esValido()) {
 			if (this.edad >= edadJuniorMin && this.edad <= edadJuniorMax) {
 				categoria = "Junior";
 			} else if (this.edad >= edadSeniorMin && this.edad <= edadSeniorMax) {
@@ -101,4 +114,66 @@ public class Jugador {
 		}
 		return categoria;
 	}
+	//Cierre del metodo
+
+	
+	
+	 /**
+
+     * Método que sirve para obtener la validez de un jugador
+	 * @return Un boolean que define si el jugador es valido 
+
+     */
+	
+	
+	public boolean esValido() {
+		int enteroNull = -1;
+		boolean valido = false;
+		if (nombre != null && edad != enteroNull && idioma != null) {
+			valido = true;
+		}
+		return valido;
+
+	}
+	//Cierre del metodo
+	
+	 /**
+
+     * Método que devuelve el idioma del jugador
+	 * @return Un string que define el idioma del jugador
+
+     */
+
+	public String getIdioma() {
+		return idioma;
+	}
+	
+	//Cierre del metodo
+	
+	 /**
+
+     * Método que devuelve el nombre del jugador
+	 * @return Un string que define el nombre del jugador
+
+     */
+
+
+	public String getNombreJugador() {
+		return nombre;
+	}
+	//Cierre del metodo
+	
+
+	 /**
+
+     * Método que devuelve la edad del jugador
+	 * @return Un int que define la edad del jugador
+
+     */
+
+	public int getEdad() {
+		return edad;
+	}
+	//Cierre del metodo
+
 }
