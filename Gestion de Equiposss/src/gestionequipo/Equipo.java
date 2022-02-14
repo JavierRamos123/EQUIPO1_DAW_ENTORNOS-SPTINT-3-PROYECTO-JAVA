a530d7940d8b44fd8ebede8941273df09775d4bc
Delgao11 <91954337+Delgao11@users.noreply.github.com>
2022-02-14 11:14:25 +0100

    javadoc
    ds

--- Gestion de Equiposss/src/gestionequipo/Equipo.java
+++ Gestion de Equiposss/src/gestionequipo/Equipo.java
@@ -1,6 +1,8 @@
 package gestionequipo;
+
 /**
  * Esta clase define Equipos
+ * 
  * @author: Equipo1DAW
  * @version 13/02/2022/A
  */
@@ -8,74 +10,81 @@ public class Equipo {
 	// Atributos de la clase
 	private String nombreEquipo;
 	private int ranking;
-	   /**
-     * Constructor para la serie Equipo
-     */
+
+	/**
+	 * Constructor para la serie Equipo
+	 */
 	public Equipo() {
 	}
-	//Cierre del constructor
-	
-		 /**
+	// Cierre del constructor
 
-	     * M�todo que sirve para asignarle un nombre a un Equipo
-	     * @param nombreEquipo El par�metro nombreEquipo define el nombre del Equipo
-
-	     */
+	/**
+	 * 
+	 * M�todo que sirve para asignarle un nombre a un Equipo
+	 * 
+	 * @param nombreEquipo El par�metro nombreEquipo define el nombre del Equipo
+	 * 
+	 */
 	public void setNombreEquipo(String nombreEquipo) {
 		this.nombreEquipo = null;
-        if(nombreEquipo != null) {
-            if (nombreEquipo.length()>=4 && nombreEquipo.length()<=20) {
-                if (nombreEquipo.matches("[a-zA-Z]+")){       
-                    this.nombreEquipo=nombreEquipo.toUpperCase();
-                }
-            }
-        }
-	}//Cierre del metodo
-	
-	 /**
+		if (nombreEquipo != null) {
+			if (nombreEquipo.length() >= 4 && nombreEquipo.length() <= 20) {
+				if (nombreEquipo.matches("[a-zA-Z]+")) {
+					this.nombreEquipo = nombreEquipo.toUpperCase();
+				}
+			}
+		}
+	}// Cierre del metodo
 
-    * M�todo que sirve para asignarle El Ranking a un Equipo
-    * @param ranking El par�metro ranking define la  posicion del equipo
-
-    */
-
-	
+	/**
+	 * 
+	 * M�todo que sirve para asignarle El Ranking a un Equipo
+	 * 
+	 * @param ranking El par�metro ranking define la posicion del equipo
+	 * 
+	 */
 
 	public void setRanking(int ranking) {
 		int enteroNull = -1;
 		int valorMin = 0;
 		int valorMax = 10;
-		if(ranking>=valorMin && ranking<=valorMax) {
-		}else {
-			ranking=enteroNull;
+		if (ranking >= valorMin && ranking <= valorMax) {
+		} else {
+			ranking = enteroNull;
 		}
 		this.ranking = ranking;
-	}//Cierre del metodo
-	 /**
+	}// Cierre del metodo
 
-     * M�todo que sirve para devolver el nombre del Equipo
-     * @param nombreEquipo El par�metro nombreEquipo define el nombre del Equipo
-
-     */
+	/**
+	 * 
+	 * M�todo que sirve para devolver el nombre del Equipo
+	 * 
+	 * @param nombreEquipo El par�metro nombreEquipo define el nombre del Equipo
+	 * 
+	 */
 	public String getNombreEquipo() {
 		return nombreEquipo;
 	}
-	//Cierre del metodo
-		 /**
 
-	     * M�todo que sirve para devolver el ranking del Equipo
-	     * @param ranking El par�metro ranking define el ranking del Equipo
-
-	     */
+	// Cierre del metodo
+	/**
+	 * 
+	 * M�todo que sirve para devolver el ranking del Equipo
+	 * 
+	 * @param ranking El par�metro ranking define el ranking del Equipo
+	 * 
+	 */
 	public int getRanking() {
 		return ranking;
 	}
-	 /**
 
-	    * M�todo que sirve para  asignar la categoria a un Equipo
-	    * @param categoria El par�metro categoria define la  categoria del Equipo
-
-	    */
+	/**
+	 * 
+	 * M�todo que sirve para asignar la categoria a un Equipo
+	 * 
+	 * @param categoria El par�metro categoria define la categoria del Equipo
+	 * 
+	 */
 
 	public String categoriaEquipo() {
 		String categoria = null;
@@ -86,30 +95,33 @@ public class Equipo {
 		int rankingTerceroMin = 3;
 
 		if (esValidoEquipo()) {
-			if(this.ranking>= rankingPrimeroMin && this.ranking<=rankingPrimeroMax) {
+			if (this.ranking >= rankingPrimeroMin && this.ranking <= rankingPrimeroMax) {
 				categoria = "Primera";
-			}else if(this.ranking>=rankingSegundoMin && this.ranking<=rankingSegundoMax) {
+			} else if (this.ranking >= rankingSegundoMin && this.ranking <= rankingSegundoMax) {
 				categoria = "Segunda";
-			}else if(this.ranking<rankingTerceroMin) {
+			} else if (this.ranking < rankingTerceroMin) {
 				categoria = "Tercera";
 			}
-		}	//Cierre del metodo
+		} // Cierre del metodo
 		return categoria;
-		 /**
-
-		    * Metodo que sirve para  comprobar la validez   de nombreEquipo y ranking
-		    * @param valido El parametro  valido comprueba si nombreEquipo y ranking son validos
-
-		    */
+		/**
+		 * 
+		 * Metodo que sirve para comprobar la validez de nombreEquipo y ranking
+		 * 
+		 * @param valido El parametro valido comprueba si nombreEquipo y ranking son
+		 *               validos
+		 * 
+		 */
 
 	}
-	public boolean esValidoEquipo() {
-        int enteroNull=-1;
-        boolean valido=false;
-        if (nombreEquipo != null && ranking!= enteroNull ) {
-            valido = true;
-        }
-        return valido;
 
-    }//Cierre del metodo
+	public boolean esValidoEquipo() {
+		int enteroNull = -1;
+		boolean valido = false;
+		if (nombreEquipo != null && ranking != enteroNull) {
+			valido = true;
+		}
+		return valido;
+
+	}// Cierre del metodo
 }
