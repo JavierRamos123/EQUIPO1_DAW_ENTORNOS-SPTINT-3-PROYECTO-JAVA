962ee044ccbdd15c9c58945eb02e19a3f2bbb79d
Delgao11 <91954337+Delgao11@users.noreply.github.com>
2022-02-14 11:02:31 +0100

    EquipoJavadoc

--- Gestion de Equiposss/src/gestionequipo/Equipo.java
+++ Gestion de Equiposss/src/gestionequipo/Equipo.java
@@ -1,13 +1,27 @@
 
 package gestionequipo;
-
+/**
+ * Esta clase define Equipos
+ * @author: Equipo1DAW
+ * @version 13/02/2022/A
+ */
 public class Equipo {
+	// Atributos de la clase
 	private String nombreEquipo;
 	private int ranking;
-
+	   /**
+     * Constructor para la serie Equipo
+     */
 	public Equipo() {
 	}
+	//Cierre del constructor
+	
+		 /**
 
+	     * Método que sirve para asignarle un nombre a un Equipo
+	     * @param nombreEquipo El parámetro nombreEquipo define el nombre del Equipo
+
+	     */
 	public void setNombreEquipo(String nombreEquipo) {
 		this.nombreEquipo = null;
         if(nombreEquipo != null) {
@@ -17,7 +31,14 @@ public class Equipo {
                 }
             }
         }
-	}
+	}//Cierre del metodo
+	
+	 /**
+
+    * Método que sirve para asignarle El Ranking a un Equipo
+    * @param ranking El parámetro ranking define la  posicion del equipo
+
+    */
 
 	
 
@@ -30,15 +51,32 @@ public class Equipo {
 			ranking=enteroNull;
 		}
 		this.ranking = ranking;
-	}
+	}//Cierre del metodo
+	 /**
 
+     * Método que sirve para devolver el nombre del Equipo
+     * @param nombreEquipo El parámetro nombreEquipo define el nombre del Equipo
+
+     */
 	public String getNombreEquipo() {
 		return nombreEquipo;
 	}
+	//Cierre del metodo
+		 /**
 
+	     * Método que sirve para devolver el ranking del Equipo
+	     * @param ranking El parámetro ranking define el ranking del Equipo
+
+	     */
 	public int getRanking() {
 		return ranking;
 	}
+	 /**
+
+	    * Método que sirve para  asignar la categoria a un Equipo
+	    * @param categoria El parámetro categoria define la  categoria del Equipo
+
+	    */
 
 	public String categoriaEquipo() {
 		String categoria = null;
@@ -59,7 +97,14 @@ public class Equipo {
 		}
 		return categoria;
 
-	}
+	}//Cierre del metodo
+	 /**
+
+	    * Método que sirve para  comprobar la validez del  nombre del Equipo y ranking
+	    * @param valido El parámetro valido define la  categoria del Equipo
+
+	    */
+
 	public boolean esValido() {
         int enteroNull=-1;
         boolean valido=false;
@@ -68,5 +113,5 @@ public class Equipo {
         }
         return valido;
 
-    }
+    }//Cierre del metodo
 }
